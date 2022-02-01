7e1be6bab8f41676f40559a1af281e65e3b79080
Michael Browne <mr.michael.browne@gmail.com>
2022-02-01 20:50:30 +0100

    Refactored auth middleware to throw unauthorized exception if the user in the token does not exist in the database. Previously threw not found.

--- src/api/Fig.Api/Middleware/AuthMiddleware.cs
+++ src/api/Fig.Api/Middleware/AuthMiddleware.cs
@@ -1,4 +1,5 @@
 using Fig.Api.Authorization;
+using Fig.Api.Exceptions;
 using Fig.Api.Services;
 
 namespace Fig.Api.Middleware;
@@ -17,15 +18,22 @@ public class AuthMiddleware
         ISettingsService settingsService,
         ITokenHandler tokenHandler)
     {
-        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-        var userId = tokenHandler.Validate(token);
-        if (userId != null)
+        try
         {
-            // attach user to context on successful jwt validation
-            var user = userService.GetById(userId.Value);
-            context.Items["User"] = user;
-            userService.SetAuthenticatedUser(user);
-            settingsService.SetAuthenticatedUser(user);
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var userId = tokenHandler.Validate(token);
+            if (userId != null)
+            {
+                // attach user to context on successful jwt validation
+                var user = userService.GetById(userId.Value);
+                context.Items["User"] = user;
+                userService.SetAuthenticatedUser(user);
+                settingsService.SetAuthenticatedUser(user);
+            }
+        }
+        catch (UnknownUserException)
+        {
+            throw new UnauthorizedAccessException("User in provided authorization token is not known");
         }
 
         await _next(context);
