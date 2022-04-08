d484b2a0a234ac6d6bc2f5a2b080a0733eafa316
Michael Browne <mr.michael.browne@gmail.com>
2022-04-08 21:26:28 +0200

    Add manage account page and control

---
+++ src/web/Fig.Web/Pages/ManageAccount.razor.cs
@@ -0,0 +1,63 @@
+using Fig.Contracts.Authentication;
+using Fig.Web.Models.Authentication;
+using Fig.Web.Notifications;
+using Fig.Web.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace Fig.Web.Pages;
+
+public partial class ManageAccount
+{
+    [Inject]
+    IAccountService AccountService { get; set; } = null!;
+    
+    [Inject]
+    private NotificationService NotificationService { get; set; } = null!;
+
+    [Inject]
+    private INotificationFactory NotificationFactory { get; set; } = null!;
+
+    private bool _showPasswordRow;
+    private string _password = string.Empty;
+    private bool _passwordValid;
+    
+    private void Submit(AuthenticatedUserModel user)
+    {
+        var request = new UpdateUserRequestDataContract
+        {
+            Username = user.Username,
+            FirstName = user.FirstName,
+            LastName = user.LastName
+        };
+
+        if (_passwordValid)
+            request.Password = _password;
+
+        if (user.Id != null)
+        {
+            AccountService.Update(user.Id.Value, request);
+            NotificationService.Notify(NotificationFactory.Success("User Updated", "Details updated successfully."));
+        }
+            
+    }
+
+    private void OnValidPassword(string password)
+    {
+        _passwordValid = true;
+        _password = password;
+        StateHasChanged();
+        Console.WriteLine($"Updating password to {password}. PasswordValid: {_passwordValid}");
+    }
+
+    private void ShowPasswordRow()
+    {
+        _showPasswordRow = true;
+    }
+
+    private void OnInvalidPassword(string password)
+    {
+        _passwordValid = false;
+        StateHasChanged();
+    }
+}
