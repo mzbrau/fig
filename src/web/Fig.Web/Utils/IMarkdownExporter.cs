459bdc6516a145e4e576c25edc83e09d2df586a4
Michael Browne <mr.michael.browne@gmail.com>
2022-06-25 23:15:04 +0200

    Removed renamed files

--- src/web/Fig.Web/Utils/IMarkdownExporter.cs
+++
@@ -1,8 +0,0 @@
-using Fig.Contracts.ImportExport;
-
-namespace Fig.Web.Utils;
-
-public interface IMarkdownExporter
-{
-    string CreateMarkdown(FigDataExportDataContract export, bool maskSecrets);
-}
