e16f1bfa7d20eb1b798b63450cfa3c19ffa70502
Michael Browne <mr.michael.browne@gmail.com>
2022-12-20 21:55:38 +1100

    Add Algolia search for documentation website

--- doc/fig-documentation/docusaurus.config.js
+++ doc/fig-documentation/docusaurus.config.js
@@ -137,7 +137,30 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark'
-      }
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'A1WOHSQ91H',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '178e02980223381b845ab27d6ffa7461',
+  
+        indexName: 'figsettings',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Algolia search parameters
+        //searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 };
 
