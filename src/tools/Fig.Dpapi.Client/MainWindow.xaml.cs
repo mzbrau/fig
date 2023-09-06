f5d11d8021700c281eacd2dac2af943ffb4b825d
Michael Browne <michael.browne1@saabgroup.com>
2023-09-06 23:25:03 +0200

    Add dpapi client which creates the encrypted texts

---
+++ src/tools/Fig.Dpapi.Client/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fig.Dpapi.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel model;
+        
+        public MainWindow()
+        {
+            model = new ViewModel();
+            DataContext = model;
+            InitializeComponent();
+        }
+
+        private void OnCopyEncryptedText(object sender, RoutedEventArgs e)
+        {
+            model.CopyEncryptedText();
+        }
+
+        private void OnGenerateSecret(object sender, RoutedEventArgs e)
+        {
+            model.GenerateSecret();
+        }
+    }
+}
---
+++ src/tools/Fig.Dpapi.Client/ViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace Fig.Dpapi.Client;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    private string _plainText;
+    private string _encryptedText;
+    private bool supressEvents;
+
+    public string PlainText
+    {
+        get => _plainText;
+        set
+        {
+            if (value != _plainText)
+            {
+                _plainText = value;
+                EncryptedText = EncryptText(_plainText);
+                if (!supressEvents)
+                {
+                    supressEvents = true;
+                    OnPropertyChanged();
+                    supressEvents = false;
+                }
+            }
+        }
+    }
+
+    public string EncryptedText
+    {
+        get => _encryptedText;
+        set
+        {
+            if (value != _encryptedText)
+            {
+
+                _encryptedText = value;
+                PlainText = DecryptText(_encryptedText);
+                if (!supressEvents)
+                {
+                    supressEvents = true;
+                    OnPropertyChanged();
+                    supressEvents = false;
+                }
+            }
+        }
+    }
+
+    private string EncryptText(string plainText)
+    {
+        var bytes = Encoding.UTF8.GetBytes(plainText);
+        var protectedData = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
+        return Convert.ToBase64String(protectedData);
+    }
+
+    private string DecryptText(string encryptedText)
+    {
+        return Encoding.UTF8.GetString(
+                ProtectedData.Unprotect(Convert.FromBase64String(encryptedText), null, DataProtectionScope.CurrentUser));
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public void CopyEncryptedText()
+    {
+        Clipboard.SetText(EncryptedText);
+    }
+
+    public void GenerateSecret()
+    {
+        PlainText = Guid.NewGuid().ToString("N");
+    }
+}
