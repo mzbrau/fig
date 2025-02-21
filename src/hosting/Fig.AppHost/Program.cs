a7ce38a37aae751570be63d4a412afcff73b5380
Michael Browne <michael.browne1@saabgroup.com>
2025-02-21 13:27:34 +0100

    Add environment variables for Fig API

--- src/hosting/Fig.AppHost/Program.cs
+++ src/hosting/Fig.AppHost/Program.cs
@@ -8,12 +8,15 @@ builder.AddProject<Fig_Api>("fig-api")
 builder.AddProject<Fig_Web>("fig-web");
 
 builder.AddProject<Fig_Examples_AspNetApi>("aspnetapi-example")
+    .WithEnvironment("FIG_API_URI", "https://localhost:7281")
     .WithArgs("--secret=f984efe5b49b40ffaf53428cec9530b8");
 
 builder.AddProject<Fig_Integration_ConsoleWebHookHandler>("console-webhook")
+    .WithEnvironment("FIG_API_URI", "https://localhost:7281")
     .WithArgs("--secret=0352ee79afb2451aaf5733e047bd6c69");
 
 builder.AddProject<Fig_Integration_SqlLookupTableService>("sql-lookup-table")
+    .WithEnvironment("FIG_API_URI", "https://localhost:7281")
     .WithArgs("--secret=3422fdd2cffd4c9d9e67d1a13e146ca3")
     .WithHttpsEndpoint(7040);
 
