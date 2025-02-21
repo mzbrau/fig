33018de7ce5cafbc3fd2337cdb0d53fc95484d51
Michael Browne <michael.browne1@saabgroup.com>
2025-02-21 13:26:22 +0100

    Ensure setting change repository does not add multiple entries

--- src/api/Fig.Api/Datalayer/Repositories/SettingChangeRepository.cs
+++ src/api/Fig.Api/Datalayer/Repositories/SettingChangeRepository.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Diagnostics;
 using Fig.Api.Observability;
 using Fig.Datalayer.BusinessEntities;
@@ -8,6 +9,8 @@ namespace Fig.Api.Datalayer.Repositories;
 
 public class SettingChangeRepository : RepositoryBase<SettingChangeBusinessEntity>, ISettingChangeRepository
 {
+    private static readonly object _lock = new object();
+
     public SettingChangeRepository(ISession session) 
         : base(session)
     {
@@ -16,27 +19,38 @@ public class SettingChangeRepository : RepositoryBase<SettingChangeBusinessEntit
     public async Task<SettingChangeBusinessEntity?> GetLastChange()
     {
         using Activity? activity = ApiActivitySource.Instance.StartActivity();
-        var criteria = Session.CreateCriteria<SettingChangeBusinessEntity>();
-        criteria.SetLockMode(LockMode.Upgrade);
-        var result = await criteria.UniqueResultAsync<SettingChangeBusinessEntity>();
+        var result = await Session.QueryOver<SettingChangeBusinessEntity>()
+            .Lock().Upgrade
+            .SingleOrDefaultAsync();
         return result;
     }
 
     public async Task RegisterChange()
     {
-        var item = await GetLastChange();
-        if (item is null)
+        using var transaction = Session.BeginTransaction(IsolationLevel.Serializable);
+        try
         {
-            item = new SettingChangeBusinessEntity
+            var item = await GetLastChange();
+            if (item is null)
             {
-                LastChange = DateTime.UtcNow
-            };
-            await Save(item);
+                item = new SettingChangeBusinessEntity
+                {
+                    LastChange = DateTime.UtcNow
+                };
+                await Save(item);
+            }
+            else
+            {
+                item.LastChange = DateTime.UtcNow;
+                await Update(item);
+            }
+
+            await transaction.CommitAsync();
         }
-        else
+        catch (Exception)
         {
-            item.LastChange = DateTime.UtcNow;
-            await Update(item);
+            await transaction.RollbackAsync();
+            throw;
         }
     }
 }
