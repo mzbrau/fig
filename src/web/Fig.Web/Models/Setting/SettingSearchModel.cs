4534a7c652b186eb1633713d27c89138ff201c22
Michael Browne <mr.michael.browne@gmail.com>
2025-05-09 23:10:34 +0200

    Update styling for dropdown template

--- src/web/Fig.Web/Models/Setting/SettingSearchModel.cs
+++ src/web/Fig.Web/Models/Setting/SettingSearchModel.cs
@@ -5,19 +5,20 @@ public class SettingSearchModel
     public SettingSearchModel(string clientName, string? instanceName, string settingName)
     {
         ClientName = clientName;
-        InstanceName = instanceName ?? string.Empty;
         SettingName = settingName;
     }
 
     public string ClientName { get; }
-    
-    public string InstanceName { get; }
+
+    public string InstanceName { get; } = "Instance1";
     
     public string SettingName { get; }
 
     public string CategoryColor { get; } = "Red";
 
-    public string Description { get; } = "This setting is good...";
+    public string Description { get; } = "This is a long description that has lots of lines in it";
 
     public string SettingType { get; } = "String";
+    
+    public string SettingValue { get; } = "This is a setting value and a";
 }
