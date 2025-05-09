3c633d7ed79753700b15e5731c11b61024c3270b
Michael Browne <mr.michael.browne@gmail.com>
2025-05-09 21:03:48 +0200

    Add more styling improvements

--- src/web/Fig.Web/Models/Setting/SettingSearchModel.cs
+++ src/web/Fig.Web/Models/Setting/SettingSearchModel.cs
@@ -5,13 +5,19 @@ public class SettingSearchModel
     public SettingSearchModel(string clientName, string? instanceName, string settingName)
     {
         ClientName = clientName;
-        InstanceName = instanceName;
+        InstanceName = instanceName ?? string.Empty;
         SettingName = settingName;
     }
 
     public string ClientName { get; }
     
-    public string? InstanceName { get; }
+    public string InstanceName { get; }
     
     public string SettingName { get; }
+
+    public string CategoryColor { get; } = "Red";
+
+    public string Description { get; } = "This setting is good...";
+
+    public string SettingType { get; } = "String";
 }
