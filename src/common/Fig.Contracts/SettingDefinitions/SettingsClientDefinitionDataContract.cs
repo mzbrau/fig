a43ac78b1f6771a8e870c976202695c55c3bce31
Michael Browne <mr.michael.browne@gmail.com>
2025-06-10 22:24:37 +0200

    Restore backwards compatibility between v2 clients and v1 api

--- src/common/Fig.Contracts/SettingDefinitions/SettingsClientDefinitionDataContract.cs
+++ src/common/Fig.Contracts/SettingDefinitions/SettingsClientDefinitionDataContract.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Fig.Contracts.CustomActions;
 using Fig.Contracts.Settings;
+using Fig.Contracts.SettingVerification;
 
 namespace Fig.Contracts.SettingDefinitions
 {
@@ -36,5 +38,8 @@ namespace Fig.Contracts.SettingDefinitions
         public List<CustomActionDefinitionDataContract> CustomActions { get; }
         
         public IEnumerable<SettingDataContract> ClientSettingOverrides { get; set; }
+        
+        [Obsolete("Removed in Fig 2.0")]
+        public List<SettingVerificationDefinitionDataContract> Verifications { get; } = [];
     }
 }
