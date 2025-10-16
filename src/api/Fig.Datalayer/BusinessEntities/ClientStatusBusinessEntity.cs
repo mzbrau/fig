4cc7ac6f67d7d5312bec5306675a2a65b36bbce9
Michael Browne <mr.michael.browne@gmail.com>
2025-10-16 20:06:25 +0200

    Add tooltip to clients with no running instances to show the last time they were online

--- src/api/Fig.Datalayer/BusinessEntities/ClientStatusBusinessEntity.cs
+++ src/api/Fig.Datalayer/BusinessEntities/ClientStatusBusinessEntity.cs
@@ -3,4 +3,7 @@ namespace Fig.Datalayer.BusinessEntities;
 // ReSharper disable once ClassNeverInstantiated.Global used by NHibernate
 public class ClientStatusBusinessEntity : ClientBase
 {
+    public virtual DateTime? LastRunSessionDisconnected { get; set; }
+
+    public virtual string? LastRunSessionMachineName { get; set; }
 }
