f45f9228535e57f866cd976b108417ccc9869fb8
Michael Browne <mr.michael.browne@gmail.com>
2026-01-23 14:45:21 +0100

    implement review comments

--- src/api/Fig.Api/Datalayer/Repositories/SettingClientRepository.cs
+++ src/api/Fig.Api/Datalayer/Repositories/SettingClientRepository.cs
@@ -116,31 +116,6 @@ public class SettingClientRepository : RepositoryBase<SettingClientBusinessEntit
         return clients;
     }
 
-    public async Task<IList<SettingClientBusinessEntity>> GetAllInstancesOfClientReadOnly(string name)
-    {
-        using Activity? activity = ApiActivitySource.Instance.StartActivity();
-        var criteria = Session.CreateCriteria<SettingClientBusinessEntity>();
-        criteria.Add(Restrictions.Eq(nameof(SettingClientBusinessEntity.Name), name));
-        // No LockMode.Upgrade for read-only operations to reduce contention
-        var clients = (await criteria.ListAsync<SettingClientBusinessEntity>()).ToList();
-
-        Parallel.ForEach(clients, 
-            new ParallelOptions { MaxDegreeOfParallelism = 4 },
-            c =>
-            {
-                c.DeserializeAndDecrypt(_encryptionService);
-                c.ValidateCodeHash(_codeHasher, _logger);
-            });
-        
-        // Evict entities since they won't be modified
-        foreach (var client in clients)
-        {
-            await Session.EvictAsync(client);
-        }
-        
-        return clients;
-    }
-
     public async Task DeleteClient(SettingClientBusinessEntity client)
     {
         await Delete(client);
